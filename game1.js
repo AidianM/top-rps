6b301f7f238986b164f736fd214bacec37d13aa7
Aidian <AidianMMXX@gmail.com>
2022-03-06 15:31:21 -0600

    Big update. GUI from tutorial and custom.

--- game1.js
+++ game1.js
@@ -1,12 +1,16 @@
+//to do: cheat codes (Christina/c added as ++ default win), fix console reporting of full choice name with abbreviated inputs
+
 function game1() {
     let  playerScore=0;
     let computerScore=0;
     game();
 
+//randomization for computerPlay:
+
     function computerPlay(){
         let max=4;
         let min=1;
-        let randomNumber = Math.floor(Math.random() * (max - min) + min);
+        let randomNumber = Math.floor(Math.random() * (max - min) + min);   //0.24*(4-1)+1=1.72 floored is 1(rock), 0.88*(4-1)+1=3.64 is 3(scissors) etc
         switch (randomNumber){
             case 1:
                 return ("rock");
@@ -17,6 +21,7 @@ function game1() {
         }
     }
 
+//One round of the game:
 
     function playRound (playerSelection, computerSelection)  {
         console.log("What is your choice?");
@@ -24,8 +29,11 @@ function game1() {
         console.log("You picked " + playerSelection+ ".");
         console.log("The computer picked " +computerSelection+".");
 
+    //playerSelection vs computerSelection compare & score:
+
         switch  (playerSelection) {
             case "rock":
+            case "r":
                 switch(computerSelection) {
                     case "rock":
                         console.log ("It's a tie.");
@@ -41,6 +49,7 @@ function game1() {
                 }
                 break;
             case "paper":
+            case "p":
                 switch(computerSelection) {
                     case "rock":
                         console.log ("You win!");
@@ -56,6 +65,7 @@ function game1() {
                 }
                 break;
             case "scissors":
+            case "s":
                 switch (computerSelection){
                     case "rock":
                         console.log ("Sorry, you lose.");
@@ -69,19 +79,31 @@ function game1() {
                         console.log ("It's a tie.");
                         break;
                 }
-             break;
+                break;
+            case "christina":
+            case "c":
+                console.log ("Smart. You literally can't lose with Christina.");
+                playerScore+= 100;
+                break;
+            default:
+                console.log ("You chose an invalid selection. You forfeit this round.")
+                computerScore++;
+                break;
         }
     console.log (" ");
     console.log ("Player score: "+playerScore);
     console.log ("Computer score: "+computerScore);
     }
 
+
+//overall game framework, calling rounds for (i<=rounds) and reporting scores and end result:
+
     function game(){
         for (let i=1; i<=5; i++) {
             console.log(" ");
-            console.log("--Round# "+i+"--")
-            const playerSelection = prompt("Please pick rock, paper, or scissors.").toLowerCase();
-            const computerSelection  = computerPlay();
+            console.log("--Round # "+i+"--");
+            const playerSelection = prompt("Please pick rock, paper, or scissors.").toLowerCase(); //modifying this input to reflect "r" as "rock" would be best
+            const computerSelection = computerPlay();
             playRound (playerSelection, computerSelection);
         }
         console.log(" ");
@@ -96,11 +118,45 @@ function game1() {
     }
 
 
+//attempting the "r = rock" output swap:
+/*
+let playerInput= prompt("Please pick rock, paper, or scissors.").toLowerCase();
+    switch (playerInput){
+        case "r":
+        case "rock":
+            playerInput = "rock";
+            break;
+        case "p":
+        case "paper":
+            return "paper";
+            break;
+        case "s":
+        case "scissors":
+            return "scissors";
+            break;
+    }
 
+Another attempt:
+Somehow putting this in makes everything come up as "ROCK" regardless of input.
+In retrospect I didn't put this inside a function, as my brain is dumb. Next step is to try it with "function choiceConvert(){"
 
+if (playerInput === "r" || "rock"){
+    playerSelection = "rock";
+} else if (playerInput === "p" || "paper"){
+    playerSelection = "paper";
+} else if (playerInput === "s" || "scissors"){
+    playerSelection = "scissors";
+} else if (playerInput === "c" || "christina"){
+    playerSelection = christina;
+} else {
+    playerSelection = "invalid";
+}
+*/
 
+//putting the above below playerSelection and changing the playRound() switch case did not work. Maybe a full if/else run needed.
 
-
+//"Which comes up most often?" We could use the Math.random to run and log hundreds of attempts and keep a record as a secondary
+//project, could be a fun rework exercise. Same for heads/tails, with the ++ going to counters based on the parameters
 
 
 
---
+++ gameCustom.js
@@ -0,0 +1,91 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '🪨',
+        beats: 'scissors',
+        category: 'all'
+    },
+    {
+        name: 'paper',
+        emoji: '📜',
+        beats: 'rock',
+        category: 'all'
+    },
+    {
+        name: 'scissors',
+        emoji: '✂️',
+        beats: 'paper',
+        category: 'all'
+    },
+    {
+        name: 'xtina',
+        emoji: '💖',
+        beats: 'all'
+    }
+]
+
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+       const selectionName = selectionButton.dataset.selection
+      const selection = SELECTIONS.find(selection => selection.name === selectionName)
+       makeSelection(selection)
+    })
+})
+
+function makeSelection(selection){
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (computerWinner) incrementScore(computerScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText)+1
+}
+
+function addSelectionResult(selection, winner){
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+
+
+}
+
+function isWinner(selection, opponentSelection){
+   /* return selection.beats === opponentSelection.name */
+
+    if (selection.name === 'xtina') {
+       return selection.beats === opponentSelection.category
+    } else {
+        return selection.beats === opponentSelection.name
+    }
+}
+
+function randomSelection(){
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length-1)
+    return SELECTIONS[randomIndex]
+}
+
+/*added cheat modes below*/
+
+function cheating() {
+    /*alert("Smart choice, you can't lose with Christina.");*/
+    const div = document.createElement('div')
+    div.innerText = xtina.emoji
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+
+}
